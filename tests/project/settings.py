7f6091f6e905fa1a481379a4cfc4d4b2a8099f80
Matti Lamppu <matti.lamppu@vincit.fi>
2024-07-22 08:19:42 +0300

    Update deps

--- tests/project/settings.py
+++ tests/project/settings.py
@@ -33,7 +33,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "graphiql_debug_toolbar.middleware.DebugToolbarMiddleware",
+    # TODO: Broken, see https://github.com/flavors/django-graphiql-debug-toolbar/pull/27
+    # "graphiql_debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
