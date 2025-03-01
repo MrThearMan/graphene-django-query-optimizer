7f498e7023e11723f81365115019156cdabf2b55
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2025-03-01 09:40:10 +0200

    Update settings

--- query_optimizer/settings.py
+++ query_optimizer/settings.py
@@ -20,20 +20,15 @@ SETTING_NAME: str = "GRAPHQL_QUERY_OPTIMIZER"
 
 
 class DefaultSettings(NamedTuple):
-    OPTIMIZER_MARK: str = "_optimized"
-    """Key used mark if a queryset has been optimized by the query optimizer."""
+    ALLOW_CONNECTION_AS_DEFAULT_NESTED_TO_MANY_FIELD: bool = False
+    """
+    Should DjangoConnectionField be allowed to be generated for nested to-many fields
+    if the ObjectType has a connection? If False (default), always use DjangoListFields.
+    Doesn't prevent defining a DjangoConnectionField on the ObjectType manually.
+    """
 
-    PREFETCH_COUNT_KEY: str = "_optimizer_count"
-    """Name used for annotating the prefetched queryset total count."""
-
-    PREFETCH_SLICE_START: str = "_optimizer_slice_start"
-    """Name used for aliasing the prefetched queryset slice start."""
-
-    PREFETCH_SLICE_STOP: str = "_optimizer_slice_stop"
-    """Name used for aliasing the prefetched queryset slice end."""
-
-    PREFETCH_PARTITION_INDEX: str = "_optimizer_partition_index"
-    """Name used for aliasing the prefetched queryset partition index."""
+    DEFAULT_FILTERSET_CLASS: str = ""
+    """The default filterset class to use."""
 
     DISABLE_ONLY_FIELDS_OPTIMIZATION: bool = False
     """Disable optimizing fetched fields with `queryset.only()`."""
@@ -41,21 +36,26 @@ class DefaultSettings(NamedTuple):
     MAX_COMPLEXITY: int = 10
     """Default max number of 'select_related' and 'prefetch related' joins optimizer is allowed to optimize."""
 
+    OPTIMIZER_MARK: str = "_optimized"
+    """Key used mark if a queryset has been optimized by the query optimizer."""
+
+    PREFETCH_COUNT_KEY: str = "_optimizer_count"
+    """Name used for annotating the prefetched queryset total count."""
+
+    PREFETCH_PARTITION_INDEX: str = "_optimizer_partition_index"
+    """Name used for aliasing the prefetched queryset partition index."""
+
+    PREFETCH_SLICE_START: str = "_optimizer_slice_start"
+    """Name used for aliasing the prefetched queryset slice start."""
+
+    PREFETCH_SLICE_STOP: str = "_optimizer_slice_stop"
+    """Name used for aliasing the prefetched queryset slice end."""
+
     SKIP_OPTIMIZATION_ON_ERROR: bool = False
     """If there is an unexpected error, should the optimizer skip optimization (True) or throw an error (False)?"""
 
-    DEFAULT_FILTERSET_CLASS: str = ""
-    """The default filterset class to use."""
-
     TOTAL_COUNT_FIELD: str = "totalCount"
     """The field name to use for fetching total count in connection fields."""
-
-    ALLOW_CONNECTION_AS_DEFAULT_NESTED_TO_MANY_FIELD: bool = False
-    """
-    Should DjangoConnectionField be allowed to be generated for nested to-many fields
-    if the ObjectType has a connection? If False (default), always use DjangoListFields.
-    Doesn't prevent defining a DjangoConnectionField on the ObjectType manually.
-    """
 
 
 DEFAULTS: dict[str, Any] = DefaultSettings()._asdict()
