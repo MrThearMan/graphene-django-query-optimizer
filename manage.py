07a5ecf2ff3c7185c2f3e3a9fa7a8ad391b88c7e
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2025-09-03 19:38:56 +0300

    Fix django settings refs

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_project.project.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_project.config.settings")
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
